f62660d0b70311dfd18eebc2a1042a0a8fa40735
Pedro <joaopedropereira2209@gmail.com>
2026-09-24 15:58:23 -0300

    docs: document the category mapper

--- src/main/java/com/io/github/pedroolivsz/cardapio/category/mapper/CategoryMapper.java
+++ src/main/java/com/io/github/pedroolivsz/cardapio/category/mapper/CategoryMapper.java
@@ -1,10 +1,20 @@
 package com.io.github.pedroolivsz.cardapio.category.mapper;
 
-import com.io.github.pedroolivsz.cardapio.category.dto.CategoryRequestDTO;
+import com.io.github.pedroolivsz.cardapio.category.dto.CategoryRequest;
 import com.io.github.pedroolivsz.cardapio.category.entity.Category;
 
+/**
+ * Classe responsável por converter objetos relacionados
+ * à entidade Category.
+ */
 public class CategoryMapper {
-    public static Category toEntity(CategoryRequestDTO categoryRequestDTO) {
-        return new Category(categoryRequestDTO.name());
+    /**
+     * Converte um FoodRequest em uma entidade Food.
+     *
+     * @param categoryRequest dados da categoria recebidos pela API
+     * @return entidade Category preenchida com os dados fornecidos
+     */
+    public static Category toEntity(CategoryRequest categoryRequest) {
+        return new Category(categoryRequest.name());
     }
 }
