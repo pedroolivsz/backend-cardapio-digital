36ea9da01856a4fd375c4058f85063d739a93628
Pedro <joaopedropereira2209@gmail.com>
2026-09-24 16:00:49 -0300

    docs: document the menu controller

--- src/main/java/com/io/github/pedroolivsz/cardapio/menu/controller/MenuController.java
+++ src/main/java/com/io/github/pedroolivsz/cardapio/menu/controller/MenuController.java
@@ -1,7 +1,10 @@
 package com.io.github.pedroolivsz.cardapio.menu.controller;
 
-import com.io.github.pedroolivsz.cardapio.category.dto.CategoryMenuDTO;
+import com.io.github.pedroolivsz.cardapio.category.dto.CategoryMenu;
 import com.io.github.pedroolivsz.cardapio.menu.service.MenuService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +14,17 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/menu")
-@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:3000"}, allowedHeaders = "*")
+@CrossOrigin(
+        origins = {
+                "http://localhost:5173",
+                "http://localhost:3000"
+        },
+        allowedHeaders = "*"
+)
+@Tag(
+        name = "Menu",
+        description = "Operações relacionadas ao menu"
+)
 public class MenuController {
     private final MenuService menuService;
 
@@ -19,8 +32,16 @@ public class MenuController {
         this.menuService = menuService;
     }
 
+    @Operation(
+            summary = "Listar categorias e os alimentos relacionados a ela",
+            description = "Retorna todos as categorias e alimentos cadastrados"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Lista de categorias e alimentos retornada com sucesso"
+    )
     @GetMapping
-    public List<CategoryMenuDTO> getMenu() {
+    public List<CategoryMenu> getMenu() {
         return menuService.getMenu();
     }
 }
