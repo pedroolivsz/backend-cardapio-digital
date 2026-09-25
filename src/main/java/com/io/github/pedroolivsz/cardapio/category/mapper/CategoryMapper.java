59e4b36d450c36462b3eb5b63bc853eb9fe9efdf
Pedro <joaopedropereira2209@gmail.com>
2026-09-25 18:42:36 -0300

    feat: add entity to response method

--- src/main/java/com/io/github/pedroolivsz/cardapio/category/mapper/CategoryMapper.java
+++ src/main/java/com/io/github/pedroolivsz/cardapio/category/mapper/CategoryMapper.java
@@ -1,6 +1,7 @@
 package com.io.github.pedroolivsz.cardapio.category.mapper;
 
 import com.io.github.pedroolivsz.cardapio.category.dto.CategoryRequest;
+import com.io.github.pedroolivsz.cardapio.category.dto.CategoryResponse;
 import com.io.github.pedroolivsz.cardapio.category.entity.Category;
 
 /**
@@ -9,12 +10,22 @@ import com.io.github.pedroolivsz.cardapio.category.entity.Category;
  */
 public class CategoryMapper {
     /**
-     * Converte um FoodRequest em uma entidade Food.
+     * Converte uma CategoryRequest em uma entidade Category.
      *
      * @param categoryRequest dados da categoria recebidos pela API
      * @return entidade Category preenchida com os dados fornecidos
      */
     public static Category toEntity(CategoryRequest categoryRequest) {
         return new Category(categoryRequest.name());
+    }
+
+    /**
+     * Converte uma Category em uma entidade CategoryResponse.
+     *
+     * @param category dados da categoria recebidos
+     * @return CategoryResponse preenchida com os dados fornecidos
+     */
+    public static CategoryResponse toResponse(Category category) {
+        return new CategoryResponse(category.getId(), category.getName());
     }
 }
